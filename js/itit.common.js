3d721aa7fa8ee36d72e66311058772d8944b2ca0
ginko.wang <yingosen@gmail.com>
2018-04-26 11:25:47 +0800

    1.upgrade echarts 2.bugfix for new version echarts

--- js/itit.common.js
+++ js/itit.common.js
@@ -69,6 +69,46 @@
 })(window, jQuery);
 // Add Extends for JS Object
 (function () {
+    Object.isTypeOf = function (object, type) {
+        return typeof(object) === type;
+    };
+    Object.isUndefined = function (object) {
+        return typeof(object) === "undefined";
+    };
+    Object.isBoolean = function (object) {
+        return typeof(object) === "boolean";
+    };
+    Object.isNumber = function (object) {
+        return typeof(object) === "number";
+    };
+    Object.isString = function (object) {
+        return typeof(object) === "string";
+    };
+    Object.isFunction = function (object) {
+        return typeof(object) === "function";
+    };
+    Object.isObject = function (object) {
+        return typeof(object) === "object";
+    };
+    Object.isNotNullObject = function (object) {
+        return typeof(object) === "object" && object !== null;
+    };
+    Object.isNull = function (object) {
+        return object === null;
+    };
+    Object.isArray = function (object) {
+        return Array.isArray(object);
+    };
+    Object.methods = function (object) {
+        var methods = [];
+        for (let prop in object) {
+            if (object[prop] && object[prop].constructor &&
+                object[prop].call && object[prop].apply) {
+                methods.push(prop);
+            }
+        }
+        return methods;
+    };
     Date.prototype.format = function (fmt) { // author: meizz
         var o = {
             "M+": this.getMonth() + 1, // 月份
@@ -79,119 +119,185 @@
             "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
             "S": this.getMilliseconds() // 毫秒
         };
-        if (/(y+)/.test(fmt))
+        if (/(y+)/.test(fmt)) {
             fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "")
                 .substr(4 - RegExp.$1.length));
+        }
         for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt))
-                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k])
+            if (new RegExp("(" + k + ")").test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k])
                     : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
         return fmt;
     };
-    Number.prototype.format = function (precision, separator) {
-        return Number.format(this, precision, separator);
-    };
-
-    Number.format = function (num, precision, separator) {
-        var parts = [];
-        // 判断是否为数字
-        if (!isNaN(parseFloat(num)) && isFinite(num)) {
-            // 把类似 .5, 5. 之类的数据转化成0.5, 5, 为数据精度处理做准, 至于为什么
-            // 不在判断中直接写 if (!isNaN(num = parseFloat(num)) && isFinite(num))
-            // 是因为parseFloat有一个奇怪的精度问题, 比如 parseFloat(12312312.1234567119)
-            // 的值变成了 12312312.123456713
-            num = Number(num);
-            // 处理小数点位数
-            num = (typeof precision !== 'undefined' ? num.toFixed(precision) : num).toString();
-            // 分离数字的小数部分和整数部分
-            parts = num.split('.');
-            // 整数部分加[separator]分隔, 借用一个著名的正则表达式
-            parts[0] = parts[0].toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1' + (separator || ','));
-            return parts.join('.');
-        }
-        return NaN;
+    Date.isDate = function (date) {
+        return (date instanceof Date && !isNaN(date.valueOf()));
     };
     Date.format = function (jsondate, fmt) {
+        if (isNaN(parseInt(jsondate))) {
+            return "--";
+        }
         return new Date(jsondate).format(fmt);
     };
+    Date.nowString = function (radix) {
+        var _radis = parseInt(radix);
+        if (isNaN(_radis)) {
+            _radis = 36;
+        }
+        return new Date().getTime().toString(_radis);
+    };
+    Number.integer = {
+        min: -2147483648,
+        max: 2147483647
+    };
     /**
-     * 秒时间格式化
-     * @param seconds
-     * @returns {string}
+     * 数字检查
+     *
+     * @param value
+     * @param min
+     *            option
+     * @param max
+     *            option
      */
-    Date.formatSecond = function (value) {
-        var _diff = "-:-";
-        if (typeof(value) !== "number") {
-            return _diff;
+    Number.check = function (value, min, max) {
+        var ret = false;
+        var _value = parseFloat(value);
+        if (isNaN(_value) || _value != value) {
+            return ret;
         }
-        var _val = [];
-        var _second = value % 60;
-        var _minute = parseInt(value / 60) % 60;
-        var _hour = parseInt(value / 60 / 60) % 60;
-        if (_hour > 0) {
-            if (_hour < 10) {
-                _val.push("0");
+        ret = true;
+        var _min = parseFloat(min);
+        var _max = parseFloat(max);
+        if (!isNaN(_min)) {
+            if (_min.toString() !== min.toString()) {
+                return false;
             }
-            _val.push(_hour);
-            _val.push(":");
+            ret = _value >= _min;
+            if (!ret) {
+                return false;
+            }
         }
-        if (_minute < 10) {
-            _val.push("0");
+        if (!isNaN(_max)) {
+            if (_max.toString() !== max.toString()) {
+                return false;
+            }
+            ret = _value <= _max;
         }
-        _val.push(_minute);
-        _val.push(":");
-        if (_second < 10) {
-            _val.push("0");
+        return ret;
+    };
+    Number.isNumeric = function (value) {
+        return !isNaN(parseFloat(value)) && !isNaN(value - parseFloat(value));
+    };
+    Number.parseIntWithDefault = function (value, vdefault) {
+        vdefault = parseInt(vdefault);
+        if (isNaN(vdefault)) {
+            vdefault = 0;
         }
-        _val.push(_second);
-        _diff = _val.join("");
-        return _diff;
-    };
-    Date.dateDiff = function (jsonDate, now) {
-        var date = new Date(parseInt(value, 10));
-        var diffDay = Math.floor((now.getTime() - date.getTime()) / 1000);
-        var _diff = Date.format(value, "yyyy-MM-dd hh:mm");
-        if (diffDay < 60) {
-            _diff = "一分钟前"
-        } else if (diffDay < 5 * 60) {
-            _diff = "五分钟前"
-        } else if (diffDay < 10 * 60) {
-            _diff = "十分钟前"
-        } else if (diffDay < 20 * 60) {
-            _diff = "二十分钟前"
-        } else if (diffDay < 30 * 60) {
-            _diff = "三十分钟前"
-        } else if (diffDay < 60 * 60) {
-            _diff = "一小时前"
-        } else if (diffDay < 2 * 60 * 60) {
-            _diff = "两小时前"
-        } else if (diffDay < 3 * 60 * 60) {
-            _diff = "三小时前"
-        } else if (diffDay < 12 * 60 * 60) {
-            _diff = "半天前"
+        value = parseInt(value);
+        if (isNaN(value)) {
+            value = vdefault;
         }
-        return _diff;
+        return value;
     };
-    String.prototype.replaceAll = function (target, replacement) {
-        return this.split(target).join(replacement);
-    };
-    String.prototype.trim = function () {
-        return this.replace(/(^\s*)|(\s*$)/g, "");
-    };
-    String.prototype.ltrim = function () {
-        return this.replace(/(^\s*)/g, "");
-    };
-    String.prototype.rtrim = function () {
-        return this.replace(/(\s*$)/g, "");
-    };
-    String.prototype.startsWith = function (searchString, position) {
-        position = position || 0;
-        return this.indexOf(searchString, position) === position;
-    };
-    String.prototype.endWith = function (s) {
-        if (!s || s == "" || this.length == 0 || s.length > this.length) {
-            return false
+    Number.parseIntRate = function (value, rate) {
+        if (!Number.isNumeric(value)) {
+            return 0;
         }
-        return (this.substring(this.length - s.length) == s);
+        if (!Number.isNumeric(rate)) {
+            rate = 1;
+        }
+        return parseInt((value * rate).toPrecision(12));
+    };
+    Number.parseIntRatio = function (value, ratio) {
+        var _ratio = Number.parseIntWithDefault(ratio, 0);
+        if (_ratio === 0) {
+            throw new Error("ratio can't be zero!!");
+        }
+        return parseInt((value / _ratio).toPrecision(12));
+    };
+    Number.parseFloatWithDefault = function (value, vdefault, toFixed) {
+        vdefault = parseFloat(vdefault);
+        if (isNaN(vdefault)) {
+            vdefault = 0;
+        }
+        toFixed = parseInt(toFixed);
+        if (isNaN(toFixed)) {
+            toFixed = -1;
+        }
+        value = parseFloat(value);
+        if (isNaN(value)) {
+            value = vdefault;
+        }
+        value = value.toPrecision(12);
+        if (toFixed > -1) {
+            return parseFloat(parseFloat(value).toPrecision(toFixed));
+        }
+        return value;
+    };
+    Number.pad = function (number, len) {
+        return (Array(len).join(0) + number).slice(-len);
+    };
+    Number.formatPenny = function (value) {
+        value = parseFloat(value);
+        if (isNaN(value)) {
+            value = 0;
+        }
+        return Number(value / 100).toFixed(2);
+    };
+    Number.formatRate = function (value, rate, toFixed) {
+        value = parseFloat(value);
+        rate = parseInt(rate);
+        toFixed = parseInt(toFixed);
+        if (isNaN(toFixed)) {
+            toFixed = 2;
+        }
+        if (isNaN(value)) {
+            return "--";
+        }
+        if (isNaN(rate)) {
+            rate = 1;
+        }
+        return Number(value / rate).toFixed(toFixed);
+    };
+    String.isEmpty = function (value) {
+        return !value || value.toString().trim().length === 0;
+    };
+    String.isMobile = function (mobile) {
+        let reg = /^1[0-9]{10}$/;
+        return reg.test(mobile);
+    };
+    String.isEmail = function (email) {
+        let reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return reg.test(email);
+    };
+    String.isUrl = function (url) {
+        let reg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
+        return reg.test(url);
+    };
+
+    if (!Array.isArray) {
+        Array.isArray = function (arg) {
+            return Object.prototype.toString.call(arg) === '[object Array]';
+        };
+    }
+    Array.isEmpty = function (array) {
+        return !array || !Array.isArray(array) || array.length === 0;
+    };
+    Array.deleteElement = function (array, callback) {
+        if (!Array.isArray(array) || array.length === 0) {
+            return array;
+        }
+        if (!Object.isTypeOf(callback, "function")) {
+            throw new Error("callback can not be null");
+        }
+        let _newList = [];
+        array.forEach(function (item, index) {
+            let _match = callback.apply(null, [item, index]);
+            if (_match === true) {
+                return;
+            }
+            _newList.push(item);
+        });
+        return _newList;
     };
 })();
